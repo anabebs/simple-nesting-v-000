41109b4d2ab44a42576e66e012c0ea9f156da74e
Ana Bebic <anabebic.x@gmail.com>
2018-07-03 12:55:34 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -17,10 +17,12 @@ def hopper
      }
 
 programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	
+	
 	
 	programmer_hash = 
  		{
@@ -38,6 +40,7 @@ def alan_kay_is_known_for
         }
      }
 end
+programmer_hash[:alan_kay,:known_for]
 
 def dennis_ritchies_language
 	programmer_hash = 
